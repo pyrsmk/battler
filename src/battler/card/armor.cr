380215324de0078a86515af271247288b5e8d654
Aurélien Delogu <aurelien.delogu@gmail.com>
2026-06-30 09:20:06 +0200

    Fix durable weapon stats in C1 mode

--- src/battler/card/armor.cr
+++ src/battler/card/armor.cr
@@ -12,9 +12,9 @@ module Battler
             "Race"     => @data[:race].to_s
           },
           {
-            "DF bonus" => @data[:race] == Race::DurableArmor ?
-                          "#{@data[:df] * 100} / #{(@data[:df] - 10) * 100}" :
-                          (@data[:df] * 100).to_s
+            "DF bonus" => @data[:race] == Race::DurableArmor && @data[:df] <= 10 ?
+                          (@data[:df] * 100).to_s :
+                          "#{@data[:df] * 100} / #{(@data[:df] - 10) * 100}"
           },
           {
             "Special"  => SPECIAL_DESCRIPTIONS[@data[:special]]? || "Unknown (#{@data[:special]})"
